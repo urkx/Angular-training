72cc5c281c0a6823d82cfa5eced74b3c24e1cbd6
Urko López <urkolopezlopez@gmail.com>
2021-08-24 10:35:18 +0200

    Heroe Component added.
    Navigation between Heroes an Heroe component.

--- udemy/02-spa/src/app/components/heroes/heroes.component.ts
+++ udemy/02-spa/src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from 'src/app/services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -9,11 +10,17 @@ export class HeroesComponent implements OnInit {
 
   heroes: Heroe[] = [];
 
-  constructor( private _heroesService: HeroesService ) { console.log('Constructor') }
+  constructor( private _heroesService: HeroesService, private _router: Router ) { //console.log('Constructor') 
+  }
 
   ngOnInit(): void {
     this.heroes = this._heroesService.getHeroes();
-    console.log(this.heroes);
+    //console.log(this.heroes);
+  }
+
+  verHeroe(idx: number)
+  {
+    this._router.navigate(['/heroe',idx]);
   }
 
 }
