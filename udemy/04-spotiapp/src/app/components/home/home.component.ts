bd7decab611d1fc85ba786466a6a0165ae743069
Urko López <urkolopezlopez@gmail.com>
2021-08-25 18:01:57 +0200

    Error managing in home component

--- udemy/04-spotiapp/src/app/components/home/home.component.ts
+++ udemy/04-spotiapp/src/app/components/home/home.component.ts
@@ -11,16 +11,27 @@ export class HomeComponent implements OnInit {
 
   nuevasCanciones: any[] = [];
   loading: boolean;
+  error: boolean;
+  mensajeError: string;
 
   constructor( private _spotify: SpotifyService) { 
     this.loading = true;
+    this.error = false;
 
     this._spotify.getNewReleases()
-    .subscribe( (data: any) => {
+    .subscribe( 
+        (data: any) => 
+        {
 
-      this.nuevasCanciones = data;
+          this.nuevasCanciones = data;
 
-    } );
+        }, 
+        (error) => 
+        {
+          this.error = true;
+          this.mensajeError = error.error.error.message;
+        } 
+    );
     
     this.loading = false;
   }
