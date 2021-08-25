f377ddd1d947b217ce11728fbf9c4bd66e894451
Urko López <urkolopezlopez@gmail.com>
2021-08-25 14:00:56 +0200

    Tarjetas componen created

--- udemy/04-spotiapp/src/app/app.module.ts
+++ udemy/04-spotiapp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { ROUTES } from './app.routes';
 import { SpotifyService } from './services/spotify.service';
 import { NoimagePipe } from './pipes/noimage.pipe';
+import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { NoimagePipe } from './pipes/noimage.pipe';
     SearchComponent,
     ArtistaComponent,
     NavbarComponent,
-    NoimagePipe
+    NoimagePipe,
+    TarjetasComponent
   ],
   imports: [
     BrowserModule,
