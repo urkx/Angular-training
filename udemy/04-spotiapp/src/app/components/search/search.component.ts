f3007b3d67af5b590585edbfc89b6b2f0f6aacf3
Urko López <urkolopezlopez@gmail.com>
2021-08-25 10:37:16 +0200

    Search component

--- udemy/04-spotiapp/src/app/components/search/search.component.ts
+++ udemy/04-spotiapp/src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-search',
@@ -8,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  artistas: any[] = [];
+
+  constructor(private _spotify: SpotifyService) { }
 
   ngOnInit(): void {
+  }
+
+  buscar(termino: string){
+    console.log(termino);
+    this._spotify.getArtista(termino)
+      .subscribe( (data: any) => {
+        console.log(data.artists.items);
+        this.artistas = data.artists.items;
+      } );
   }
 
 }
--- udemy/04-spotiapp/src/app/services/spotify.service.ts
+++ udemy/04-spotiapp/src/app/services/spotify.service.ts
@@ -19,4 +19,14 @@ export class SpotifyService {
 
     return this._http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', {headers});
   }
+
+  getArtista(termino: string){
+    const headers = new HttpHeaders(
+      {
+        'Authorization': 'Bearer BQDgsEcvtVcoaU6CqfM6Jmgyjj5fhsAjD0uBUXpLg8mO-e--6cII3v_YPn6221_dxf9-bWWxLb9IFvLMyDA'
+      }
+    );
+
+    return this._http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers});
+  }
 }
