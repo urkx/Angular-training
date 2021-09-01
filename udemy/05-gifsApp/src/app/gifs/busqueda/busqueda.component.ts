cb93762d9ee873952d9a1ffa69407db6096bfea7
Urko López <urkolopezlopez@gmail.com>
2021-09-01 13:44:02 +0200

    Search box action

--- udemy/05-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
+++ udemy/05-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -8,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusquedaComponent implements OnInit {
 
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  buscar(){
+    const valor = this.txtBuscar.nativeElement.value;
+
+    console.log(valor);
+
+    this.txtBuscar.nativeElement.value = '';
   }
 
 }
