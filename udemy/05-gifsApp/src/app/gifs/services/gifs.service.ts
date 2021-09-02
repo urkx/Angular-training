717ab74075ef797e98727e02e529da53f7c07dac
Urko López <urkolopezlopez@gmail.com>
2021-09-02 12:52:04 +0200

    History stored in local storage

--- udemy/05-gifsApp/src/app/gifs/services/gifs.service.ts
+++ udemy/05-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -12,7 +12,13 @@ export class GifsService {
 
   public resultados: Gif[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+    
+    
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [] ;
+    
+
+  }
 
   get historial()
   {
@@ -26,6 +32,8 @@ export class GifsService {
     {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
 
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`)
