ef49772686965b5d6a424fd66f794193dde0652f
Urko López <urkolopezlopez@gmail.com>
2021-09-06 13:06:40 +0200

    por-pais component

--- udemy/paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ udemy/paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -8,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorPaisComponent implements OnInit {
 
+  termino: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  buscar(){
+    console.log(this.termino);
   }
 
 }
