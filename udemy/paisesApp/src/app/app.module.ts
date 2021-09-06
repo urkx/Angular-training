ffa2dbd86debe7b61b8af9f1c7987739c2f17cc5
Urko López <urkolopezlopez@gmail.com>
2021-09-06 11:45:42 +0200

    Some modules and basic app.component HTML

--- udemy/paisesApp/src/app/app.module.ts
+++ udemy/paisesApp/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { PaisModule } from './pais/pais.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    PaisModule
   ],
   providers: [],
   bootstrap: [AppComponent]
