5dfd7b79a8c244c0a01a8563343f18596143c4f2
Urko López <urkolopezlopez@gmail.com>
2021-09-07 11:34:33 +0200

    ver pais page

--- udemy/paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ udemy/paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
@@ -8,9 +12,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor() { }
+  pais!: Country;
+
+  constructor(private activatedRoute: ActivatedRoute, private paisService: PaisService) { }
 
   ngOnInit(): void {
+
+    this.activatedRoute.params
+      .pipe( 
+        switchMap((params) => this.paisService.buscarPaisPorCodigo(params.id)), 
+        tap(console.log) 
+      )
+      .subscribe(pais => this.pais = pais);
+
+    // this.activatedRoute.params.subscribe(({id}) => {  // {id} === params.id
+    //   this.paisService.buscarPaisPorCodigo(id)
+    //   .subscribe(pais => {
+    //     console.log(pais);
+    //   });
+    // })
   }
 
 }
--- udemy/paisesApp/src/app/pais/services/pais.service.ts
+++ udemy/paisesApp/src/app/pais/services/pais.service.ts
@@ -28,4 +28,11 @@ export class PaisService {
     return this.http.get<Country[]>(url);
   }
 
+  buscarPaisPorCodigo(id: string): Observable<Country>{
+    
+    const url = `${this.apiUrl}/alpha/${id}`;
+
+    return this.http.get<Country>(url);
+  }
+
 }
