84adfa4ca245fd339dd2e77342641f487b04b758
Urko López <urkolopezlopez@gmail.com>
2021-09-08 10:29:46 +0200

    Heroes routing

--- udemy/spa2/src/app/heroes/heroes.module.ts
+++ udemy/spa2/src/app/heroes/heroes.module.ts
@@ -5,13 +5,15 @@ import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
+import { HeroesRoutingModule } from './heroes-routing.module';
 
 
 
 @NgModule({
   declarations: [AgregarComponent, BuscarComponent, HeroeComponent, HomeComponent, ListadoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HeroesRoutingModule
   ]
 })
 export class HeroesModule { }
