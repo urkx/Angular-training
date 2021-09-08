30f627b8fa57e9efa9680af75a7144cc9f6020f1
Urko López <urkolopezlopez@gmail.com>
2021-09-08 11:45:52 +0200

    Angular material components: sidenav, toolbar and button

--- udemy/spa2/src/app/heroes/heroes.module.ts
+++ udemy/spa2/src/app/heroes/heroes.module.ts
@@ -6,6 +6,8 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../material/material.module';
 
 
 
@@ -13,7 +15,9 @@ import { HeroesRoutingModule } from './heroes-routing.module';
   declarations: [AgregarComponent, BuscarComponent, HeroeComponent, HomeComponent, ListadoComponent],
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    HeroesRoutingModule,
+    FlexLayoutModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }
