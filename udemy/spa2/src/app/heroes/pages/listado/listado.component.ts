2bdc8a0ac0ab0557c087c7d78b5422ae4c2057fa
Urko López <urkolopezlopez@gmail.com>
2021-09-08 13:27:15 +0200

    Heroe type and heroes list

--- udemy/spa2/src/app/heroes/pages/listado/listado.component.ts
+++ udemy/spa2/src/app/heroes/pages/listado/listado.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-listado',
@@ -8,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadoComponent implements OnInit {
 
-  constructor() { }
+  heroes: Heroe[] = []
+
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
+    this.heroesService.getHeroes().subscribe(res => this.heroes = res);
   }
 
 }
