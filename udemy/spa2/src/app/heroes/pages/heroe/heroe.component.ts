8a562cbdca3de3aa8ec12377acf5062865205962
Urko López <urkolopezlopez@gmail.com>
2021-09-13 12:53:27 +0200

    Heroe page

--- udemy/spa2/src/app/heroes/pages/heroe/heroe.component.ts
+++ udemy/spa2/src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Heroe } from '../../interfaces/heroes.interface';
 import { switchMap } from 'rxjs/operators';
 import { HeroesService } from '../../services/heroes.service';
@@ -7,19 +7,29 @@ import { HeroesService } from '../../services/heroes.service';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
+  styles: [`
+      img{
+        width: 100%;
+        border-radius: 5px;
+      }
+  `
   ]
 })
 export class HeroeComponent implements OnInit {
 
   heroe!: Heroe;
 
-  constructor(private activatedRoute: ActivatedRoute, private heroesService: HeroesService) { }
+  constructor(private activatedRoute: ActivatedRoute, private heroesService: HeroesService, private router: Router) { }
 
   ngOnInit(): void {
     this.activatedRoute.params
     .pipe(switchMap(({id}) => this.heroesService.getHeroePorId(id)))
     .subscribe((heroe) => this.heroe = heroe);
+  }
+
+  regresar()
+  {
+    this.router.navigate(['/heroes/listado']);
   }
 
 }
