f9b13bf1f8383263cfb572ecf50d8adceda543a7
Urko López <urkolopezlopez@gmail.com>
2021-09-13 12:58:22 +0200

    environment variable for baseUrl API

--- udemy/spa2/src/app/heroes/services/heroes.service.ts
+++ udemy/spa2/src/app/heroes/services/heroes.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { Heroe } from '../interfaces/heroes.interface';
 
 @Injectable({
@@ -7,15 +8,17 @@ import { Heroe } from '../interfaces/heroes.interface';
 })
 export class HeroesService {
 
+  private baseUrl: string = environment.baseUrl;
+
   constructor(private http: HttpClient) { }
 
   getHeroes(){
 
-    return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes`);
 
   }
 
   getHeroePorId(id: string){
-    return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`);
+    return this.http.get<Heroe>(`${this.baseUrl}/heroes/${id}`);
   }
 }
