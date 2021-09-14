f90976ae04b3f1f8e6f89f9149852df248ba0dc7
Urko López <urkolopezlopez@gmail.com>
2021-09-14 13:20:14 +0200

    delete heroe and not pure image pipe

--- udemy/spa2/src/app/heroes/pages/agregar/agregar.component.ts
+++ udemy/spa2/src/app/heroes/pages/agregar/agregar.component.ts
@@ -65,4 +65,11 @@ export class AgregarComponent implements OnInit {
     }
   }
 
+  borrar(){
+
+    this.heroesService.borrarHeroe(this.heroe.id!)
+          .subscribe(resp => {this.router.navigate(['/heroes'])});
+
+  }
+
 }
