b692b16fe2055bafc5fa932f91aad20465dfa04a
Urko López <urkolopezlopez@gmail.com>
2021-09-16 14:04:08 +0200

    fields validation

--- udemy/formulariosApp/src/app/template/basicos/basicos.component.ts
+++ udemy/formulariosApp/src/app/template/basicos/basicos.component.ts
@@ -20,8 +20,12 @@ export class BasicosComponent implements OnInit {
     console.log(this.miFormulario);
   }
 
-  nombreValido(){
-    return this.miFormulario?.controls.producto?.invalid && this.miFormulario?.controls.producto?.touched
+  nombreValido(): boolean{
+    return this.miFormulario?.controls.producto?.invalid && this.miFormulario?.controls.producto?.touched;
+  }
+
+  precioValido(): boolean{
+    return this.miFormulario?.controls.precio?.touched && this.miFormulario?.controls.precio?.value < 0 ;
   }
 
 }
