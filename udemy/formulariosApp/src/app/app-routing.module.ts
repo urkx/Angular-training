a14c13da3b837d285ca3d38d40ef81eeee689836
Urko López <urkolopezlopez@gmail.com>
2021-09-16 12:02:52 +0200

    Added component in reactive and template modules. Added lazyload for both modules

--- udemy/formulariosApp/src/app/app-routing.module.ts
+++ udemy/formulariosApp/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'template',
+    loadChildren: () => import('./template/template.module').then(m => m.TemplateModule)
+  },
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive.module').then(m => m.ReactiveModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'template'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
