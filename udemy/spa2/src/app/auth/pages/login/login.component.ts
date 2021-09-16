587c1d96e2f3280fdc27ca3c8d9a7b4eb292bf1a
Urko López <urkolopezlopez@gmail.com>
2021-09-16 08:57:52 +0200

    Auth

--- udemy/spa2/src/app/auth/pages/login/login.component.ts
+++ udemy/spa2/src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +10,19 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
   login(){
 
-    this.router.navigate(["./heroes"]);
+    this.authService.login().subscribe(res => {
+      if(res.id){
+        this.router.navigate(["./heroes"]); 
+      }
+    });
+    // this.router.navigate(["./heroes"]);
 
   }
 
---
+++ udemy/spa2/src/app/auth/services/auth.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Auth } from '../interfaces/auth.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl: string = environment.baseUrl;
+  private _auth: Auth | undefined;
+
+  constructor(private http: HttpClient) { }
+
+  get auth(): Auth{
+    return  {...this._auth!};
+  }
+
+  login(){
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`)
+    .pipe(tap(res => this._auth = res));
+  }
+
+}
