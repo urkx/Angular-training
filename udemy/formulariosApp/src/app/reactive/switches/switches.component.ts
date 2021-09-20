5a2cc57fcc45d7d519c8fce911d82244e9191c1f
Urko López <urkolopezlopez@gmail.com>
2021-09-20 13:26:43 +0200

    Reactive: switches

--- udemy/formulariosApp/src/app/reactive/switches/switches.component.ts
+++ udemy/formulariosApp/src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+
+  miFormulario: FormGroup = this.formBuilder.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [false, Validators.requiredTrue],
+  });
+
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  }
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({...this.persona, condiciones: true});
+
+    this.miFormulario.valueChanges.subscribe(({condiciones, ...rest}) => {
+      //delete form.condiciones;
+      this.persona = rest;
+    });
+  }
+
+  guardar(){
+    const formValue = {...this.miFormulario.value};
+    delete formValue.condiciones;
+
+    this.persona = formValue;
   }
 
 }
