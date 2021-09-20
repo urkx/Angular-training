a736697178597182159fc9a605bbe19b660617b9
Urko López <urkolopezlopez@gmail.com>
2021-09-20 16:17:03 +0200

    validators as service

--- udemy/formulariosApp/src/app/auth/registro/registro.component.ts
+++ udemy/formulariosApp/src/app/auth/registro/registro.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { emailPattern, nombreApellidoPattern, noPuedeSerPericoPalotes } from 'src/app/shared/validators/validaciones';
+import { ValidatorService } from 'src/app/shared/validators/validator.service';
 
 @Component({
   selector: 'app-registro',
@@ -11,12 +11,12 @@ import { emailPattern, nombreApellidoPattern, noPuedeSerPericoPalotes } from 'sr
 export class RegistroComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: ['', [Validators.required, Validators.pattern(nombreApellidoPattern)]],
-    email: ['', [Validators.required, Validators.pattern(emailPattern)]],
-    username: ['', [Validators.required, noPuedeSerPericoPalotes]],
+    nombre: ['', [Validators.required, Validators.pattern(this.vs.nombreApellidoPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.vs.emailPattern)]],
+    username: ['', [Validators.required, this.vs.noPuedeSerPericoPalotes]],
   });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private vs: ValidatorService) { }
 
   ngOnInit(): void {
     this.miFormulario.reset({
