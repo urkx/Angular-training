ea3b7a599b149220338f5398245803f820287ff9
Urko López <urkolopezlopez@gmail.com>
2021-09-21 12:15:09 +0200

    selectoresAnidados: initial content

---
+++ udemy/selectoresAnidados/src/app/paises/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+  styles: [
+  ]
+})
+export class SelectorPageComponent implements OnInit {
+
+  miFormulario: FormGroup = this.fb.group({
+    region: ['', [Validators.required]],
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+  guardar(){
+    console.log(this.miFormulario.value);
+  }
+
+}
