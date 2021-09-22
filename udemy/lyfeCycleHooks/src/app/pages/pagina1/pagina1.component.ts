c1af2a16657c2336327c9c79c0b319b80ab09828
Urko López <urkolopezlopez@gmail.com>
2021-09-22 08:40:02 +0200

    content

--- udemy/lyfeCycleHooks/src/app/pages/pagina1/pagina1.component.ts
+++ udemy/lyfeCycleHooks/src/app/pages/pagina1/pagina1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-pagina1',
@@ -6,11 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class Pagina1Component implements OnInit {
+export class Pagina1Component 
+implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, 
+           AfterViewInit, AfterViewChecked, OnDestroy {
 
-  constructor() { }
+  nombre: string = 'Urko';
+
+  constructor() { console.log('constructor'); }
 
   ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+
+  guardar(){
+
   }
 
 }
