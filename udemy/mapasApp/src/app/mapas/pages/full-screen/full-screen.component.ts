e382c894c68b2f072e96e315cf3f40259cb02e82
Urko López <urkolopezlopez@gmail.com>
2021-09-22 15:33:36 +0200

    map center and zoom

--- udemy/mapasApp/src/app/mapas/pages/full-screen/full-screen.component.ts
+++ udemy/mapasApp/src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
-import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-full-screen',
@@ -19,11 +18,14 @@ export class FullScreenComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    (mapboxgl as any).accessToken = environment.mapboxToken;
+
     var map = new mapboxgl.Map({
     container: 'mapa',
-    style: 'mapbox://styles/mapbox/streets-v11'
-    });
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-6.0146304, 37.4113679],
+    zoom: 14
+    }); 
+
   }
 
 }
