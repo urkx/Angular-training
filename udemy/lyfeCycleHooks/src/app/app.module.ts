1df5db3d73e3ec8b4b37e7d5fd0fbf811667effd
Urko López <urkolopezlopez@gmail.com>
2021-09-22 08:12:46 +0200

    Life cycle hooks: initial content

---
+++ udemy/lyfeCycleHooks/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { Pagina1Component } from './pages/pagina1/pagina1.component';
+import { MuestraNombreComponent } from './components/muestra-nombre/muestra-nombre.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Pagina1Component,
+    MuestraNombreComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
