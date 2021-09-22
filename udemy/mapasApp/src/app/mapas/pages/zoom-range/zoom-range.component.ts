fddd19e0048f59265f326cd8b36f9629a0c4f3fd
Urko López <urkolopezlopez@gmail.com>
2021-09-22 15:45:15 +0200

    initial design for zoom range page

--- udemy/mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ udemy/mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,9 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
+    `
+     .mapa-container{
+        height: 100%;
+        width: 100%;
+      }
+
+      .row{
+        background-color: white;
+        position: fixed;
+        bottom: 50px;
+        left: 50px;
+        padding: 10px;
+        border-radius: 5px;
+        z-index: 999999;
+      }
+    `
   ]
 })
 export class ZoomRangeComponent implements OnInit {
@@ -11,6 +28,13 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
+    var map = new mapboxgl.Map({
+    container: 'mapa',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-6.0146304, 37.4113679],
+    zoom: 14
+    }); 
   }
 
 }
